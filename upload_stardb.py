1317d5d6e8617743be6a9dca897298d0864d78be
Hashblen <hashanulblennon@gmail.com>
2024-02-06 22:29:51 +0100

    Added command-line arguments and removed cookie.py

--- upload_stardb.py
+++ upload_stardb.py
@@ -1,5 +1,15 @@
 import manage_data
 import time
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--cookie", help="set cookie", type=str)
+args = parser.parse_args()
+if args.cookie is not None:
+    manage_data.set_cookie(args.cookie)
+else:
+    print("You need to specify the cookie with --cookie. Type 'python main.py --help' for more info.")
+    exit()
 
 L = manage_data.open_completed_list()
 for chive_id in L:
